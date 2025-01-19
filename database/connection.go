143b93576f00f812afa8a680af171de4cfb1a43b
kevinherijanto24 <christopher.herijanto@student.ukdc.ac.id>
2025-01-19 20:14:14 +0700

    second commit

--- database/connection.go
+++ database/connection.go
@@ -11,7 +11,8 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	// Ganti string ini dengan string koneksi yang sudah diformat
-	dsn := "root:TKpUAYFvcQXHfmtiApbvAQTXsPzcGvoO@(autorack.proxy.rlwy.net:19382)/railway?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:admin123@tcp(aws-database-1.cv6oi4oimtxt.ap-southeast-1.rds.amazonaws.com:3306)/database-1?charset=utf8mb4&parseTime=True&loc=Local"
+
 	// Koneksi ke database menggunakan GORM
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {

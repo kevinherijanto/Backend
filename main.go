25bb48c27fe036aea11e662179bdf13d86756e40
kevinherijanto24 <christopher.herijanto@student.ukdc.ac.id>
2025-01-20 15:19:39 +0700

    second commit

--- main.go
+++ main.go
@@ -12,9 +12,6 @@ import (
 	"github.com/gofiber/websocket/v2"
 )
 
-// Global variable to store connected WebSocket clients
-var clients = make(map[*websocket.Conn]bool)
-
 func main() {
 	// Main app for HTTP routes (app1)
 	httpApp := fiber.New()
@@ -40,45 +37,15 @@ func main() {
 
 	// WebSocket route for notifications
 	wsApp.Get("/ws", websocket.New(func(c *websocket.Conn) {
-		// Add the client to the clients map
-		clients[c] = true
-		log.Println("New WebSocket connection established")
-
-		// Keep the connection alive and listen for any errors or disconnections
 		for {
-			_, _, err := c.ReadMessage()
-			if err != nil {
-				log.Println("WebSocket error:", err)
-				delete(clients, c) // Remove client from the list on disconnect
+			// Simple echo server for WebSocket
+			message := []byte("New wallet created!")
+			if err := c.WriteMessage(websocket.TextMessage, message); err != nil {
+				log.Println("Error sending WebSocket message:", err)
 				break
 			}
 		}
 	}))
-
-	// Function to send a notification to all connected clients
-	sendWalletNotification := func(message string) {
-		for client := range clients {
-			err := client.WriteMessage(websocket.TextMessage, []byte(message))
-			if err != nil {
-				log.Println("Error sending message to WebSocket client:", err)
-				client.Close()
-				delete(clients, client) // Remove client if there's an error
-			}
-		}
-	}
-
-	// Simulate wallet creation (for demonstration purposes)
-	httpApp.Post("/create-wallet", func(c *fiber.Ctx) error {
-		// Simulate wallet creation process here
-
-		// After wallet is created, send a notification to all WebSocket clients
-		sendWalletNotification("New wallet created!")
-
-		// Respond to HTTP request
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"message": "Wallet created and notification sent",
-		})
-	})
 
 	// Get the PORT environment variable for Railway deployment
 	port := os.Getenv("PORT")
